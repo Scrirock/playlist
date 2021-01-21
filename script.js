6aa96b13e056c534aa1a99cf816d0138c1fdab8a
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-21 15:29:16 +0100

    responsive

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("showTrend").addEventListener("click", ()=>{
     if (flag){
         document.getElementById("trending").style.left = "0";
         document.getElementById("trending").style.width = "25%";
-        document.getElementById("trending").style.paddingLeft = "5rem";
+        document.getElementById("trending").style.paddingLeft = "1rem";
         document.getElementById("showTrend").style.left = "25%";
         document.getElementById("showTrend").style.transform = "rotate(180deg)";
         document.getElementById("playlistContainer").style.width = "75%";
